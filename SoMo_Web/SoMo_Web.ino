3b1ed927cb288f0f86b26e40ae5f59467440dc31
Leif Bloomquist <github@leifbloomquist.net>
2015-07-16 19:02:58 -0400

    Initial Web Sketch

--- SoMo_Web/SoMo_Web.ino
+++ SoMo_Web/SoMo_Web.ino
@@ -1,9 +1,15 @@
-//SoMo Backup Code for three remote devices sending to Max (Series 1 Radios)
-/*  Unit ID for each remote device needs to be set in the global declarations using the keys below:
+// SoMo V5 sketch for SoMo Companion App
+// 
+// This sketch reads the IMU, scales values to the range 0-1024,
+// and sends them over the radio using a variant of the MAX SensorBox protocol.
+//
+// To program, ensure the Arduino IDE is set to "Arduino Leonardo" under Tools->Board
 
-// This version is Leif_Mod_V2_Final
- 
- Unit 0: 0x30
+/*This code sits on the remote devices (what the dancers will wear) 
+  in a multiple dancer setup with XBee Series 1 Radios.
+
+ Unit ID for each remote device needs to be set using the keys below:
+
  Unit 1: 0x31
  Unit 2: 0x32
  Unit 3: 0x33
@@ -25,32 +31,37 @@ int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int16_t mx, my, mz;
 
-
 #define POWER 1
 #define RX    8   //  Use 17 for SoMo V4
 #define TX    4
 
+#define RATE  20  // Every xx milliseconds.  Do not set to less than 20 as to not swamp receiver XBee
 
-SoftwareSerial mySerial(8, 4); // RX, TX
+SoftwareSerial mySerial(RX, TX); // RX, TX
 
 char analogValue[20];               //array of analog values *in byte format for MAX* plus some buffer
 
-char current=0;//current position of analog value in array
-int digVal;//digital pins bits are packed into a single variable
-char imask = 128;//index bytes start with 1
-char theEnd = 255;//byte to signal message end to Max patch
+char current = 0;    //current position of analog value in array
+int digVal   = 0;    //digital pins bits are packed into a single variable
+char imask   = 128;  //index bytes start with 0x80
+char theEnd  = 255;  //byte to signal message end
+
 //SET UNIT ID HERE
 //*******************************
-byte unitID = 0x30;
+byte unitID = 0x33;
 //*******************************
 
+// Counter for the ID packet
+int id_counter=0;
 
-void setup(void) {
-     // Turn on SoMo Power LED
+void setup(void) 
+{
+  // Turn on SoMo Power LED
   pinMode(POWER, OUTPUT);
   digitalWrite(POWER, HIGH);  
+
   Wire.begin();
-  Serial.begin(9600);  
+  Serial.begin(57600);  
 
   // initialize device
   Serial.println("Initializing I2C devices...");
@@ -60,20 +71,18 @@ void setup(void) {
   Serial.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-  mySerial.begin(9600);  //this serial corresponds to MAX PATCH
+  mySerial.begin(57600);  
+  
   digVal=0;
-  for (int i = 2;i<14;i++){
-    digitalWrite(i,HIGH);//enable pullups
+  
+  for (int i = 2;i<14;i++)
+  {
+    digitalWrite(i,HIGH);  //enable pullups
   }
-
-  while (establishContact()==0){
-    Serial.println("Waiting for connection from MAX...");
-    delay(100);
-  }  //wait for 99 byte
 }
 
-//uses serial.write() to avoid needless symbol creation in MaxMSP  
-void loop() {
+void loop() 
+{   
   // read raw accel/gyro measurements from device
   accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
 
@@ -109,7 +118,8 @@ Serial.print("a/g/m:\t");
   int scaled_mx = ScaleMAX_mag(mx);
   int scaled_my = ScaleMAX_mag(my);
   int scaled_mz = ScaleMAX_mag(mz); 
-  
+
+/*  
    // display SCALED tab-separated accel/gyro x/y/z values
   Serial.print("Scaled:\t");
   Serial.print(scaled_ax); 
@@ -129,23 +139,9 @@ Serial.print("a/g/m:\t");
   Serial.print(scaled_my); 
   Serial.print("\t");
   Serial.println(scaled_mz);
+*/
 
-  // pack the data from the MPU9150 into the SensorBox numbers to transmit
-  // currently only transmitting 6, but there are 9 of them in total
-  // the accelerometer (ax, ay, ax) and gyroscope (gx, gy, gz) are 16 bit 
-  // the magnetometer (mx, my, mz) are 13 bit
-  // SO, we remove bottom 2 bits, since we can only transmit 14
-//  packValueMPU9150(ax>>2);     //Eric added the <<2 because of the above note re: bits
-//  packValueMPU9150(ay>>2);
-//  packValueMPU9150(az>>2);
-//  packValueMPU9150(mx);
-//  packValueMPU9150(my);
-//  packValueMPU9150(mz);
-//  packValueMPU9150(gx>>2);
-//  packValueMPU9150(gy>>2);
-//  packValueMPU9150(gz>>2);
-
-  current=0;//reset analog value counter
+  current=0;  //reset analog value counter
   
   packValueMPU9150(scaled_ax); 
   packValueMPU9150(scaled_ay);
@@ -157,52 +153,40 @@ Serial.print("a/g/m:\t");
   packValueMPU9150(scaled_gy);
   packValueMPU9150(scaled_gz);
 
-  char total = current; //+1;
+  char total = current;    
+  sendMessage(total);   //send everything over radio
   
-  sendOFF(total);//send everything to Max 
-  
-  delay(10);//wait 10 milliseconds
-  //see if someone tried to turn us off:
-  if(establishContact()==1) {
-    while(establishContact()==0) {
-      delay(100);
-    }//go into idle mode
-  }
+    // Blink the power LED
+  digitalWrite(POWER, HIGH);  
+  delay(RATE);         // Wait RATE milliseconds
+  digitalWrite(POWER, LOW);     
 }
 
-void sendOFF(char total){
-  //Send analog values in the format 0x81 a1 a2.....0xFF
-  //Max wants 81 (start), FF is the end of the message
-  mySerial.write(imask|1); //this is 0x81
-  //Write UnitID to MAX
-  //**********************************
-  // Turn On/Off Serial ID
-  //mySerial.write(unitID);
- //***********************************
-  for (int i = 0;i<total;i++){
+void sendMessage(char total)
+{
+  mySerial.write(imask|0xF); //this is 0x8F
+  
+  // Send identifier phrase
+  mySerial.print("SOMOV5");
+  
+  // Send UnitID
+  mySerial.write(unitID);
+
+  // Send Analog Values
+  for (int i = 0;i<total;i++)
+  {
     mySerial.write(analogValue[i]);   
   }
-  mySerial.write(theEnd);//ends analog stream with 255
-
-/*  mySerial.write((imask|2));
+ 
+  // Send Digital Values
   mySerial.write((digVal&127));
   mySerial.write(digVal>>7);
-  mySerial.write(theEnd);//ends digital message with 255
-*/
+  
+  mySerial.write(theEnd);   //ends digital message with 255
 }
 
-//read an analog pin and then pack into low/high bytes
-/*
-void packValue(int index) {
-  int tempA = analogRead(index);
-  analogValue[current]=tempA & 127;
-  current++;
-  analogValue[current] = (tempA>>7);
-  current++;
-}
-*/
-
-void packValueMPU9150(int16_t value){
+void packValueMPU9150(int16_t value)
+{
   int16_t tempA = value; 
   analogValue[current] = tempA & 127;
   current++;
@@ -210,22 +194,10 @@ void packValueMPU9150(int16_t value){
   current++;
 }
 
-/////////////////////////////////////////
-//This looks for connection to MaxMSP
-char establishContact(void){
-  if (mySerial.available() > 0) {
-    char checkup = mySerial.read();
-    if (checkup==99) return 1;  //looking for you to click 99 in Max to run the data -- maybe comment this out
-    else return 0;
-  }
-  else return 0;
-}
-
 // Scale the inputs
 int ScaleMAX(int raw)
 {
-   float temp = Scale(raw, -16000, 16000);
-   
+   float temp = Scale(raw, -16000, 16000);   
    return temp*1024;
 }
 
@@ -233,7 +205,6 @@ int ScaleMAX(int raw)
 int ScaleMAX_mag(int raw)
 {
    float temp = Scale(raw, -32000, 32000);
-   
    return temp*1024;
 }
 
